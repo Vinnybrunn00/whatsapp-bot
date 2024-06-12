eee94c20a0eebb6604c431f0cad0ab4349b74224
Vinícius Bruno <vinibruno.dev@gmail.com>
2024-06-11 23:12:20 -0300

    v3.7

--- bot.js
+++ bot.js
@@ -588,13 +588,10 @@ async function start(bot) {
         if (message.body.startsWith('!tiktok')) {
             let args = message.body.slice(8)
             await tikdown(args).then(async url => {
-                await api.resolveDownloads(url.data.video)
-                    .then(async response => {
-                        console.log(response)
-                        if (response) {
-                            await bot.sendFile(message.from, 'src/video_tk/video.mp4')
-                            return;
-                        }
+                await api.resolveDownloads(url.data.video, message, bot)
+                    .then(async _ => {
+                        await bot.reply(message.from, 'Aguarde...', message.id);
+                        return;
                     }).catch(async err => {
                         await bot.reply(message.from, err, message.id)
                         return;
