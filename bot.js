dbb9fa4cda75133f95239ef9049243da05e76a1a
Vinicius Bruno <vinibruno.dev@gmail.com>
2024-06-28 19:14:34 -0300

    v3.8

--- bot.js
+++ bot.js
@@ -23,6 +23,7 @@ wa.create(config).then(bot => start(bot));
 
 async function start(bot) {
     bot.onMessage(async message => {
+        console.log(message)
         if (await api.isBlock(message.author)) return;
 
         let timer, timeLog;
